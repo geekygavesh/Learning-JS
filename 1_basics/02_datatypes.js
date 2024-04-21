c384706059cf1d43225f855e6eb2c7c67706189d
Gavesh <143213369+geekygavesh@users.noreply.github.com>
2024-04-21 11:04:01 +0000

    Datatypes

---
+++ 1_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) we are using node js ,not browser
+
+console.log(3 + 
+    3);  // not preferred , code readability should be high
+
+console.log("Gavesh");
+
+let name = "hello"
+let age = 18
+let isloggedIn = false
+let state ;
+
+// number 2 to power 53
+// bigint
+// string => ""
+// boolean
+// null => standalone vale
+// null ek representation h empty value ka
+// undefined => define hi nhi kiya type
+// symbol => unique
+
+// objects
+// type of me null is an object whereas undefined is undefined
+console.log(typeof null);  // o/p is object
+console.log(typeof undefined); // o/p is undefined
