9ba524140b8b9703b0524d98586ace65c6922ff4
Gavesh <143213369+geekygavesh@users.noreply.github.com>
2024-04-23 12:59:33 +0000

    Conversions

--- 1_basics/03_conversionOperation.js
+++ 1_basics/03_conversionOperation.js
@@ -22,5 +22,35 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 let somenumber = 33
 
 let stringnumber = String(somenumber);
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+// ************ Operations *************
+
+let value = 3;
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " gavesh"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);   // basically jo phle ayga vo operation krna
+// console.log(1 + 2 + "2");   // agar string phli ari to concatenate krdo else agar no. h to phle unka sum
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
