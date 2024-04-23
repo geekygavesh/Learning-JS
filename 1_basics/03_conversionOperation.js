e584fd05156eb7599393db06154db4a307b1f22d
Gavesh <143213369+geekygavesh@users.noreply.github.com>
2024-04-23 12:32:23 +0000

    Conversion of Datatypes

--- 1_basics/03_conversionOperation.js
+++ 1_basics/03_conversionOperation.js
@@ -1,6 +1,26 @@
-let score = "33abc"
+// let score = "33abc". // NaN
+let score = null // 0
+// undefined me bhi NaN
 
-console.log(typeof(score));
+//console.log(typeof(score));
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" -> 33
+// "33ab" -> NaN
+// true -> 1
+ 
+let isLoggedIn = "gavesh";
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1-> true 0-> false
+// "" -> false ;
+//  "gavesh"-> true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
