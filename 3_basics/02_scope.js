6e61254202f6206f6d1aba5eed6842a69e07ac1e
Gavesh <143213369+geekygavesh@users.noreply.github.com>
2024-05-29 20:23:45 +0000

    scope level

--- 3_basics/02_scope.js
+++ 3_basics/02_scope.js
@@ -3,9 +3,42 @@ let a = 300
 if(true){
     let a = 10;
     const b  = 20;
-    console.log(a);
+    // console.log(a);
     // var c = 30;
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "gavesh"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const username = "hitesh"
+    if(username=="hitesh"){
+        const website = " youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++++++++++  INTERESTING ++++++++++++++++++
+
+function addone(num){  // is case me declare se phle call kr skte
+    return num +1
+}
+addone(2);
+
+const addtwo = function(num){   // is case me declare se phle error ajayga (hoisting)
+   return num + 2;
+}
+addtwo(2)
