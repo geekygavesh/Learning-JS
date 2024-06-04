c1698ad0fe55024e5cd47b53ad5d710c0288b303
Gavesh <143213369+geekygavesh@users.noreply.github.com>
2024-06-04 18:22:09 +0000

    while & do-while

---
+++ 5_iterations/two.js
@@ -0,0 +1,19 @@
+let i = 3;
+while(i<=10){
+    // console.log(`value of idx is ${i}`);
+    i+=2;
+}
+
+let myarr = ["supman","ironman","thor"]
+let arr = 0;
+while(arr<myarr.length){
+    // console.log(`value is ${myarr[arr]}`);
+    arr++;
+}
+
+let score = 1
+do {
+    console.log(`score is ${score}`)
+    score++
+    
+} while (score<=10);
